ecb53d858f31194d82cfdcf3ab491f086a95a550
Rajan-rkg9 <amirajangupta@gmail.com>
2020-11-08 16:09:03 +0530

    [Rajan-rkg9] Add. multiple new employee to Json Server

--- test/java/com/capg/employeepayrollrestapi/TestEmployeePayroll.java
+++ test/java/com/capg/employeepayrollrestapi/TestEmployeePayroll.java
@@ -15,23 +15,24 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class TestEmployeePayroll {
-
+	EmployeePayrollService serviceObj;
 	@BeforeClass
 	public void setup()
 	{
 		RestAssured.baseURI="http://localhost";
 		RestAssured.port=3000;
+		serviceObj = new EmployeePayrollService();
 	}
 	public EmployeePayrollData[] getEmployeeList()
 	{
 		Response response=RestAssured.get("/employees");
-		EmployeePayrollData[] empData=new Gson().fromJson(response.asString(),EmployeePayrollData[].class);
+		EmployeePayrollData empData[] = new Gson().fromJson(response.asString(),EmployeePayrollData[].class);
 		return empData;
 	}
     @Test
     public void givenEmployeeDatainJSONServer_WhenRetrieved_ShouldMatchCount()
     {
-        EmployeePayrollData[] empData=getEmployeeList();
+        EmployeePayrollData empData[] = getEmployeeList();
         EmployeePayrollRESTAPIService restApiObj;
         restApiObj=new EmployeePayrollRESTAPIService(Arrays.asList(empData));
         long count = restApiObj.countREST_IOEntries();
@@ -40,7 +41,6 @@ public class TestEmployeePayroll {
     @Test
     public void addedNewEmployee_ShouldMatch_ResponseAndCount()
     {
-    	EmployeePayrollService serviceObj;
     	EmployeePayrollData[] empData = getEmployeeList();
     	serviceObj=new EmployeePayrollService(Arrays.asList(empData));
     	EmployeePayrollData employeePayrollData=new EmployeePayrollData(3,"Sumit",93746.0);
@@ -59,4 +59,25 @@ public class TestEmployeePayroll {
 		request.body(employeeJson);
 		return request.post("/employees");
 	}
+    
+    @Test
+    public void addedMultipleEmployees_ShouldMatch_ResponseAndCount()
+    {
+    	EmployeePayrollData empData[] = getEmployeeList();
+    	serviceObj=new EmployeePayrollService(Arrays.asList(empData));
+    	EmployeePayrollData[] newEmpRecord= {
+							    			new EmployeePayrollData(5,"Gaurav",7365454),
+							    			new EmployeePayrollData(6,"Arun",7643988),
+							    			new EmployeePayrollData(7,"Shambhu",8654433)};
+    	for(EmployeePayrollData employeePayrollData:newEmpRecord)
+    	{
+    		Response response=addEmployeeToJsonServer(employeePayrollData);
+    		int HTTPstatusCode=response.getStatusCode();
+    		assertEquals(201,HTTPstatusCode);
+    		employeePayrollData=new Gson().fromJson(response.asString(),EmployeePayrollData.class);
+        	serviceObj.addEmployeeToPayrollUsingRestAPI(employeePayrollData);
+    	}
+    	long count = serviceObj.entryCount();
+    	assertEquals(7,count);
+    }
 }
